c2ae049f6b0114353bcccfeea97fb604ffae4f80
leo004288 <leo004288@gmail.com>
2026-04-13 11:54:47 +0900

    20260413

--- hr20260413.sql
+++ hr20260413.sql
@@ -6,7 +6,7 @@
 -- 2. 함수 - FUNCTION
   -- 반드시 리턴값이 1개
 -------------------------------------------------------------------------------- 
--- 107번 직원의 이름과 월급 조회
+-- 107번 직원의 이름과 월급 조회 -----------------------------------------------
 SELECT first_name || last_name          이름,
        salary                           월급
 FROM   employees
@@ -31,13 +31,15 @@ END;
 /
 
 -- 저장 프로시저 (IN : INPUT, OUT : OUTPUT, INOUT : INPUTOUT)
+-- 파라미터는 in_empid IN NUMBER 괄호와 숫자사용 X
+-- 내부변수는 v_name 반드시 괄호와 숫자사용
 CREATE PROCEDURE get_empsal ( in_empid IN NUMBER )
 IS
 v_name VARCHAR2(46);
 v_sal  NUMBER(8, 2);
     BEGIN
         SELECT first_name || last_name, salary
-         INTO  v_name                 , v_sal
+        INTO  v_name                 , v_sal
         FROM   employees
         WHERE  employee_id = 107;
         
@@ -45,10 +47,45 @@ v_sal  NUMBER(8, 2);
     DBMS_OUTPUT.put_line('월급:' || v_sal);
     END;
 /
-DROP PROCEDURE get_empsal;
--- ORACLE로 프로시저 생성
 
--- 부서번호입력, 해당부서의 최고월급자의 이름, 월급 출력
+SET SERVEROUTPUT ON;  -- DBMS_OUTPUT.put_line() 의 결과를 출력
+CALL get_empsal(107);
+
+-- 부서번호입력, 해당부서의 최고월급자의 이름, 월급 출력 -----------------------
+CREATE OR REPLACE PROCEDURE get_name_maxsal (
+    in_deptid IN  NUMBER,
+    o_name    OUT VARCHAR2,
+    o_sal     OUT NUMBER
+    )
+IS
+    v_maxsal number(8,2);
+    BEGIN
+        SELECT MAX(salary)
+         INTO  v_maxsal
+        FROM  employees
+        WHERE department_id = in_deptid;
+        
+        SELECT first_name || last_name, salary
+         INTO  o_name                 , o_sal
+        FROM   employees
+        WHERE  salary        = v_maxsal
+         AND   department_id = in_deptid;
+         
+         DBMS_OUTPUT.put_line(o_name);
+         DBMS_OUTPUT.put_line(o_sal);
+         
+    END;
+/
+-- 테스트 -> java에서 호출해서 사용
+SET SERVEROUTPUT ON;
+VAR o_name VARCHAR2;
+VAR o_sal  NUMBER;
+CALL get_name_maxsal(90, :o_name, :o_sal);
+PRINT o_name;
+PRINT o_sal;
+
+
+
 SELECT department_id,
        first_name || last_name,
        MAX(salary)
