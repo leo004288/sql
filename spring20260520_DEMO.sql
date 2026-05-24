dc493b79ec723e25400d09a8a9da325f417ca77b
leo004288 <leo004288@gmail.com>
2026-05-24 23:11:00 +0900

    ss

--- spring20260520_DEMO.sql
+++ spring20260520_DEMO.sql
@@ -1,14 +1,20 @@
 -- PROMPT 테이블
 CREATE TABLE PROMPT (
-    Post_id    NUMBER(10, 0)      NOT NULL PRIMARY KEY,
-    cat_id     VARCHAR2(10 BYTE)  NOT NULL,
-    User_id    VARCHAR2(50 BYTE)  NOT NULL,               
-    Title      VARCHAR2(300 BYTE) NOT NULL,             
-    Content    CLOB               NOT NULL,             
-    Views      NUMBER(10, 0)      DEFAULT 0,              
-    created_at DATE               DEFAULT SYSDATE,             
-    UPDATED_AT DATE               ,             
-    IS_DELETED CHAR(1 BYTE)       DEFAULT 'N' NOT NULL            
+    id          NUMBER(10)         NOT NULL PRIMARY KEY,
+    cat_id      VARCHAR2(10 BYTE)  NOT NULL,
+    user_num    VARCHAR2(50 BYTE)  NOT NULL,               
+    title       VARCHAR2(255)      NOT NULL,             
+    content     VARCHAR2(1000)     NOT NULL,
+    img         VARCHAR2(300)      ,
+    Views       NUMBER(10)         DEFAULT 0, 
+    like_cnt    NUMBER(10, 0)      DEFAULT 0,
+    status      VARCHAR2(20)       ,
+    created_at  DATE               DEFAULT SYSDATE,             
+    updated_at  DATE               ,             
+    deleted_at  CHAR(1 BYTE)       DEFAULT 'N' NOT NULL,
+    reported_at
+    
+    CONSTRAINT FK_PROMPT_CATEGORY FOREIGN KEY (cat_id) REFERENCES PROMPT_CATEGORY (cat_id)
 );
 
 -- 카테고리 테이블
@@ -17,6 +23,12 @@ CREATE TABLE PROMPT_CATEGORY (
     cat_name VARCHAR2(100 BYTE)   NOT NULL,              
     cat_seq  VARCHAR2(100 BYTE)   NOT NULL             
 );
+
+--
+ALTER TABLE SPRING.PROMPT 
+ADD CONSTRAINT FK_PROMPT_CAT_ID
+FOREIGN KEY (cat_id) 
+REFERENCES SPRING.PROMPT_CATEGORY (cat_id);
 
 -- 트리거
 CREATE OR REPLACE TRIGGER TRG_PROMPT_UPDATE
@@ -41,6 +53,7 @@ INSERT INTO PROMPT_CATEGORY VALUES ('ETC_PMT', '기타 프롬프트', 5);
 
 -------------------------------------------------------------------------------
 COMMIT;
+
 
 DROP TABLE PROMPT_CATEGORY;
 
