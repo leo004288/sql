33a353a2dba151133494ae24f1b943b4f4a9f7a9
leo004288 <leo004288@gmail.com>
2026-06-01 17:54:00 +0900

    fif

--- spring20260520_DEMO.sql
+++ spring20260520_DEMO.sql
@@ -4,12 +4,11 @@ CREATE TABLE USERS (
     user_id         VARCHAR2(255) NOT NULL UNIQUE,
     password        VARCHAR2(255) NOT NULL,
     email           VARCHAR2(320) NOT NULL,
-    role            VARCHAR2(20)  CHECK (ROLE IN ('MEMBER', 'ADMIN')),
-    is_deleted      NUMBER(1,0)   DEFAULT 0, 
+    role            VARCHAR2(20)  CHECK (ROLE IN ('MEMBER', 'ADMIN')), 
     created_at      DATE          DEFAULT SYSDATE,
     updated_at      DATE          DEFAULT SYSDATE,
     deleted_at      DATE          ,
-    last_login_date TIMESTAMP(6)     
+    last_login_date TIMESTAMP(6)  
 );
 
 -- 카테고리 테이블
@@ -45,8 +44,8 @@ CREATE TABLE pmt_like (
     id        NUMBER(10) NOT NULL,
     user_num  NUMBER(10) NOT NULL,
     
-    CONSTRAINT FK_PMT_LIKE_ID       FOREIGN KEY (id)       REFERENCES PROMPT     (id),
-    CONSTRAINT FK_PMT_LIKE_USER_NUM FOREIGN KEY (user_num) REFERENCES users (user_num),
+    CONSTRAINT FK_PMT_LIKE_ID       FOREIGN KEY (id)       REFERENCES PROMPT     (id) ON DELETE CASCADE,
+    CONSTRAINT FK_PMT_LIKE_USER_NUM FOREIGN KEY (user_num) REFERENCES users (user_num) ON DELETE CASCADE,
     
     CONSTRAINT PK_PMT_LIKE PRIMARY KEY (id, user_num)
 );
@@ -61,6 +60,45 @@ CREATE TABLE reply (
 
     CONSTRAINT FK_REPLY_ID       FOREIGN KEY (id)       REFERENCES PROMPT (id)      ON DELETE CASCADE,
     CONSTRAINT FK_REPLY_USER_NUM FOREIGN KEY (user_num) REFERENCES users  (user_num) ON DELETE CASCADE
+);
+
+-- BOARD_CARD
+CREATE TABLE "SPRING"."BOARD_CARD" 
+(	
+    "CARD_SEQ" NUMBER, 
+    "CMENU_ID" VARCHAR2(20 BYTE) NOT NULL, 
+    "CTITLE" VARCHAR2(200 BYTE) NOT NULL, 
+    "CCONTENT" VARCHAR2(1000 BYTE), 
+    "CIMAGE_URL" VARCHAR2(500 BYTE) NOT NULL, 
+    "CLINK_URL" VARCHAR2(500 BYTE) NOT NULL, 
+    "CHIT" NUMBER DEFAULT 0, 
+    "CTAG" VARCHAR2(500 BYTE)
+);
+
+-- AI_SEARCH_WORDS
+CREATE TABLE "SPRING"."AI_SEARCH_WORDS" 
+(	
+    "WORD_SEQ" NUMBER, 
+    "CMENU_ID" VARCHAR2(20 BYTE) NOT NULL, 
+    "CTITLE" VARCHAR2(200 BYTE) NOT NULL, 
+    "SEARCH_WORD" VARCHAR2(100 BYTE) NOT NULL
+);
+
+--CATEGORY_SEARCH_WORDS
+CREATE TABLE CATEGORY_SEARCH_WORDS
+(	
+    "WORD_SEQ"      NUMBER, 
+    "CMENU_ID"      VARCHAR2(20 BYTE)  NOT NULL, 
+    "CATEGORY_NAME" VARCHAR2(100 BYTE) NOT NULL, 
+    "SEARCH_WORD"   VARCHAR2(50 BYTE)  NOT NULL
+);
+
+--USER_FAVORITE
+CREATE TABLE "SPRING"."USER_FAVORITE" 
+(	
+    "FAVORITE_ID" NUMBER, 
+    "USER_ID"     VARCHAR2(100 BYTE), 
+    "CARD_SEQ"    NUMBER
 );
 
 --------------------------------------------------------------------------------
@@ -81,29 +119,15 @@ INSERT INTO pmt_categories VALUES ('MYO_PMT', '명리학 프롬프트', 4);
 INSERT INTO pmt_categories VALUES ('ETC_PMT', '기타 프롬프트', 5);
 
 -- 유저추가
-INSERT INTO users (
-    user_num,
-    user_id,
-    password,
-    email,
-    role
-) VALUES ( 1,
-           'admin',
-           :v2,
-           :v3,
-           :v4,
-           :v5,
-           :v6,
-           :v7,
-           :v8,
-           :v9 );
+INSERT INTO users ( user_num, user_id, password, email, role) 
+VALUES ( 1, 'admin', '1234', '1234@1234.com', 'ADMIN');
 
 -- PROMPT 추가
 INSERT INTO PROMPT (id, cat_id, user_num, Title, Content)
 VALUES ((SELECT NVL(MAX(id), 0) + 1 FROM PROMPT),'MM_PMT', 1, 'sapmle', 'aiedhid');
 INSERT INTO PROMPT (id, cat_id, user_num, Title, Content)
-VALUES ((SELECT NVL(MAX(id), 0) + 1 FROM PROMPT),'EDU_PMT','admin', 'sapmle', 'aiedhid');
+VALUES ((SELECT NVL(MAX(id), 0) + 1 FROM PROMPT),'EDU_PMT', 1, 'sapmle', 'aiedhid');
 
 -------------------------------------------------------------------------------
 COMMIT;
-DROP TABLE USERS;
+DROP TABLE reply;
